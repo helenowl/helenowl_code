824c2fd4a6a858afc52226cabddbd58c0a912cdf
Helenowl <helenowl92@yandex.ru>
2019-11-23 01:01:03 +0300

    Начало диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import  s from './Dialogs.module.css';
+import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
-const Dialogs = (props) =>{
-    return(
-        <div>
-            Dialogs
+const Dialogs=(props) => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/1">Katya</NavLink>
+                </div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/2">Lena</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Valera</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>Hiiiiii</div>
+                <div className={s.message}>:))))</div>
+            </div>
+
         </div>
-    )
+    );
 
-}
+};
 
 export default Dialogs;
